31308b0667bc2e1dc7e7dd25a527cb9380c43385
pitchaimuthufb <39653052+pitchaimuthufb@users.noreply.github.com>
2022-12-28 13:37:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const router = express.Router();
 
 // Hello World for index page
 app.get('/', function (req, res) {
-    return res.send("Hello Pi World!");
+    return res.send("Hello Pi | Syed World!");
 })
 
 app.get('/api', function (req, res) {
